e375bf63c3c9d00328bcc674f49d8e32fdf09eea
David Markel <davidgmarkel@gmail.com>
2018-06-01 16:09:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -20,8 +21,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    params[:num1].to_i
-
+    params[:num1].to_i * params[:num2].to_i
   end
 
 end
